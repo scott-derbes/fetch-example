a18fa16309f00f96f74af60a7480b3c012edbe27
Scott Derbes <derbescott@gmail.com>
2024-12-12 00:43:09 -0500

    Clean up

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # fetch-example
 
 # Overview
-In order to solve the problem of reading from a Kafka stream, performing transformations, and writing to a new a Kafka topic I chose to use Apache Flink.
-Flink allowed me to easily have access to a series of tools that would allow this pipeline to scale in the event that this needed to go production.
+This is a simple Flink application to read from a Kafka stream, perform a basic aggregation, and then write to a Kafka topic. 
 
-If I were moving this to production, I would use Zookeeper instead of my current local approach to allow for High Availability in the event that a JobManager node failed. 
+The main script running the Flink code can be found in `src/aggregate_stream.py`
+
+There is also a secondary script that generates a Kafka Topic under `src/generate_kafka_topic.py`
+
+# Running Locally
+This application will require that you have Docker and Python installed on your computer. 
+
+In order to ensure your local environment is set up properly, run the command `make build_local_env`. This will generate a virtual environment and install the needed Pip packages
+
+In order to build the local Docker container, run the command `make build_local_flink_image`. This will be used for the Flink and Kafka Topic Generator Script
+
+In order to launch the Flink job run `make run_compose`. This will kick off the Docker Compose steps starting the local environment. Once the Flink job is running, the Flink UI can be accessed at [this url](http://localhost:8081/)
+
+# Additional Question Answers:
+
+1. How would you deploy this application in production?
+    - I would either use a tool like Kubernetes where I can deploy Zookeeper and Flink in their own pods or a managed service like AWS Managed Flink. I would most likely submit the jobs using the Session Mode, and then set up checkpoints and store the data off the local filesystem like S3 to allow recovery in the event that the Flink jobs dies.
+
+2. What other components would you want to add to make this production ready?
+    - I think the biggest thing this will need is better observability. In order to ensure that the service is constantly up and not being over-utilized, I would use a tool Datadog and set up alerts with Slack and Email for Task Failures and high resource utilization. 
+
+3. How can this application scale with a growing dataset?
+    - This application should scale fairly well with an increasing dataset as long as the cluster it is running on has proper autoscaling rules. Flink would just need a way to add more task nodes to handle its data processing
--- src/aggregate_stream.py
+++ src/aggregate_stream.py
@@ -1,38 +1,46 @@
 from pyflink.common import Duration, WatermarkStrategy, Types
 from pyflink.common.serialization import SimpleStringSchema
 from pyflink.datastream import StreamExecutionEnvironment, DataStream
-from pyflink.datastream.connectors.kafka import KafkaSource, KafkaSink, KafkaOffsetsInitializer, KafkaOffsetResetStrategy, KafkaRecordSerializationSchema, DeliveryGuarantee
+from pyflink.datastream.connectors.kafka import ( 
+    KafkaSource,
+    KafkaSink, 
+    KafkaOffsetsInitializer, 
+    KafkaOffsetResetStrategy,
+    KafkaRecordSerializationSchema, 
+    DeliveryGuarantee
+)
 from pyflink.datastream.formats.json import JsonRowDeserializationSchema
-from pyflink.datastream.functions import ProcessFunction, MapFunction
+from pyflink.datastream.functions import MapFunction
 from pyflink.table import DataTypes, Schema, StreamTableEnvironment, Table
 from pyflink.table.expressions import lit, col, to_timestamp_ltz
 from pyflink.table.window import Tumble
-from schema.user_login import USER_LOGIN_SCHEMA
 
 from common.config import CONFIG
 from common.logging import get_logger
+from schema.user_login import USER_LOGIN_SCHEMA
 
 logger = get_logger()
 
-class PrintWatermarkProcessFunction(ProcessFunction):
-    def process_element(self, value, ctx: ProcessFunction.Context):
-        logger.info(f"Processing Element: {value}")
-        logger.info(f"Current Watermark: {ctx.timer_service().current_watermark()}")
 
 class RowToStringMapFunction(MapFunction):
+    """Simple Map Function to convert a Row to a String
+
+    Args:
+        MapFunction (): Base Class for Map Transformation
+    """
     def map(self, value):
         return str(value)
 
 def read_kafka_source(stream_env: StreamExecutionEnvironment) -> DataStream:
     """Reads the User Login Kafka Stream, Deserializes it with specified schema, and returns it as a Datastream
 
-    Note: This function could be rebuilt to take more parameters and be able to read any Kafka Stream
+    Note: This function could be rebuilt to take more parameters and be able to read any Kafka Stream instead of just User Login
 
     Args:
-        stream_env (StreamExecutionEnvironment): _description_
+        stream_env (StreamExecutionEnvironment): Flink Stream Execution Environment
 
     Returns:
-        DataStream: _description_
+        DataStream: Stream of User Login Data from Kafka
     """
     deserilization_schema = JsonRowDeserializationSchema.builder().type_info(type_info=USER_LOGIN_SCHEMA).build()
 
@@ -51,7 +59,7 @@ def read_kafka_source(stream_env: StreamExecutionEnvironment) -> DataStream:
 
     return stream_env.from_source(kafka_source, watermark_strategy, "User Login Kafka Source").uid("read-user-login-kafka-stream")
 
-def generate_table(table_env: StreamTableEnvironment, data_stream: DataStream) -> Table:
+def generate_table(t_env: StreamTableEnvironment, data_stream: DataStream) -> Table:
     """Generates a Table from the DataStream with the specified schema, this allows for SQL operations on the stream and better control over the watermark
 
     Args:
@@ -73,21 +81,32 @@ def generate_table(table_env: StreamTableEnvironment, data_stream: DataStream) -
                 .watermark("ts", "ts - INTERVAL '5' SECOND")
                 .build()
             )
-    return table_env.from_data_stream(data_stream, schema)
+    return t_env.from_data_stream(data_stream, schema)
 
     
 def aggregate_stream(table: Table) -> Table:
+    """Aggregates the Stream by Device Type and Window. This will count the number of users for each device type in a 1 minute window
+    Very simple aggregation that generates a count of users based on device_type and locale, but shows how Table API can be used for realtime analytics
+
+    Args:
+        table (Table): Unaggregated Table
+
+    Returns:
+        Table: Aggregated Table containing the number of users for each device type in a 1 minute window
+    """
     # Coalesece None Device Types to Unknown
     table = table.select(
         col('user_id'),
         col('device_type').if_null('Unknown').alias('device_type'),
+        col('locale').if_null('Unknown').alias('locale'),
         col('ts')
     )
 
     table = table.window(Tumble.over(lit(1).minute).on(col("ts")).alias("w"))
-    table = table.group_by(col('device_type'), col('w')) \
+    table = table.group_by(col('device_type'), col('locale'), col('w')) \
         .select(
             col('device_type'), 
+            col('locale'),
             col('w').start.alias('window_start'), 
             col('w').end.alias('window_end'), 
             col('user_id').count.alias('user_count')
@@ -95,40 +114,51 @@ def aggregate_stream(table: Table) -> Table:
     
     return table
 
+def write_kafka_sink(ds: DataStream):
+    """Writes the DataStream to Kafka Topic
+
+    Args:
+        ds (DataStream): DataStream containing aggregated data
+    """
+    sink = KafkaSink.builder() \
+        .set_bootstrap_servers(CONFIG.KAFKA_BOOTSTRAP_SERVERS) \
+        .set_record_serializer(
+            KafkaRecordSerializationSchema.builder()
+                .set_topic(CONFIG.WRITE_KAFKA_TOPIC)
+                .set_value_serialization_schema(SimpleStringSchema())
+                .build()
+        ) \
+        .set_delivery_guarantee(DeliveryGuarantee.AT_LEAST_ONCE) \
+        .build()
+    
+    ds.sink_to(sink).name("Aggregate Kafka Sink")
+
 def run(stream_env: StreamExecutionEnvironment, t_env: StreamTableEnvironment):
+    """Driver function to run the Flink Job
+
+    Args:
+        stream_env (StreamExecutionEnvironment): Streaming Environment for DataStream API
+        t_env (StreamTableEnvironment): Table Environment for Table API
+    """
     ds = read_kafka_source(stream_env)
     table = generate_table(t_env, ds)
-    # table.print_schema()
     table = aggregate_stream(table)
     table.print_schema()
 
     ds = t_env.to_data_stream(table)
 
-    ds.print()
-    # ds.process(PrintWatermarkProcessFunction())
+    # Convert the Row to a String so it can be sent to Kafka
+    ds = ds.map(RowToStringMapFunction(), Types.STRING())
 
-    # # Convert the Row to a String so it can be sent to Kafka
-    # ds = ds.map(RowToStringMapFunction(), Types.STRING())
-
-    # sink = KafkaSink.builder() \
-    #     .set_bootstrap_servers(CONFIG.KAFKA_BOOTSTRAP_SERVERS) \
-    #     .set_record_serializer(
-    #         KafkaRecordSerializationSchema.builder()
-    #             .set_topic(CONFIG.WRITE_KAFKA_TOPIC)
-    #             .set_value_serialization_schema(SimpleStringSchema())
-    #             .build()
-    #     ) \
-    #     .set_delivery_guarantee(DeliveryGuarantee.AT_LEAST_ONCE) \
-    #     .build()
-    
-    # ds.sink_to(sink).name("Aggregate Kafka Sink")
-
-    stream_env.execute("Aggregate Fetch Example Stream")
+    # Write to Kafka
+    write_kafka_sink(ds)
+    stream_env.execute("Fetch Example Aggregation Stream")
 
 if __name__ == "__main__":
     streaming_env = StreamExecutionEnvironment.get_execution_environment()
+
     # start a checkpoint every 1000 ms
-    # streaming_env.enable_checkpointing(1000)
+    streaming_env.enable_checkpointing(1000)
     logger.info("Initialized Flink Stream Environment")
 
     table_env = StreamTableEnvironment.create(stream_execution_environment=streaming_env)
